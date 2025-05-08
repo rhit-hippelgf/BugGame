cee0783f349ea1acf7252882ccffb76938232aae
hippelgf <hippelgf@rose-hulman.edu>
2025-05-08 16:39:20 -0400

    Fixed Playerclass and ControllerClass

--- BugGame/src/controllerStuff/Controller.java
+++ BugGame/src/controllerStuff/Controller.java
@@ -45,4 +45,22 @@ public class Controller {
             }
         });
     }
+    
+	 private boolean up, down, left, right;
+	 public void setUp(boolean b)    { up    = b; }
+	 public void setDown(boolean b)  { down  = b; }
+	 public void setLeft(boolean b)  { left  = b; }
+	 public void setRight(boolean b) { right = b; }
+	 
+	 public void update() {
+		 int speed = 4;
+		 int dx = (right ? 1 : 0) - (left ? 1 : 0);
+		 int dy = (down  ? 1 : 0) - (up   ? 1 : 0);
+		 
+		 if (dx != 0 && dy != 0) {
+			 dx *= 0.707;  dy *= 0.707;
+		 }
+		 x += dx * speed;
+		 y += dy * speed;
+	 }
 }
--- BugGame/src/creatureStuff/Player.java
+++ BugGame/src/creatureStuff/Player.java
@@ -1,24 +1,6 @@
 package creatureStuff;
 
 public class Player extends Creature{
-	
-	 private boolean up, down, left, right;
-	 public void setUp(boolean b)    { up    = b; }
-	 public void setDown(boolean b)  { down  = b; }
-	 public void setLeft(boolean b)  { left  = b; }
-	 public void setRight(boolean b) { right = b; }
-	 
-	 public void update() {
-		 int speed = 4;
-		 int dx = (right ? 1 : 0) - (left ? 1 : 0);
-		 int dy = (down  ? 1 : 0) - (up   ? 1 : 0);
-		 
-		 if (dx != 0 && dy != 0) {
-			 dx *= 0.707;  dy *= 0.707;
-		 }
-		 x += dx * speed;
-		 y += dy * speed;
-	 }
 
 	public Player(int startX, int startY, int startSpeed, int startHealth) {
 		super(startX, startY, startSpeed, startHealth);
