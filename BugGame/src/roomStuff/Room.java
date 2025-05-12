30917aaabceb9d206fec978606f5b2d7ef003601
carioltl <carioltl@rose-hulman.edu>
2025-05-12 17:12:27 -0400

    something in room

--- BugGame/src/roomStuff/Room.java
+++ BugGame/src/roomStuff/Room.java
@@ -69,9 +69,6 @@ public class Room extends JPanel{
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		
-		Rectangle rect = new Rectangle(100,100,100,100);
-		g2.fill(rect);
 
 		if(player != null) player.draw(g2);
 
