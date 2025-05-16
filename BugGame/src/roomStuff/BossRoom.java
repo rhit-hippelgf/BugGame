29e30ef3862780477e959d880ec6512c2161524e
carioltl <carioltl@rose-hulman.edu>
2025-05-16 07:43:47 -0400

    Added classes that still need to be implemented.
    BossRoom ensures a boss is always spawned, and same with Item room.

---
+++ BugGame/src/roomStuff/BossRoom.java
@@ -0,0 +1,10 @@
+package roomStuff;
+
+public class BossRoom extends Room {
+
+	public BossRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize) {
+		super(layout, north, east, south, west, tileSize);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ BugGame/src/roomStuff/ItemRoom.java
@@ -0,0 +1,10 @@
+package roomStuff;
+
+public class ItemRoom extends Room {
+
+	public ItemRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize) {
+		super(layout, north, east, south, west, tileSize);
+		// TODO Auto-generated constructor stub
+	}
+
+}
