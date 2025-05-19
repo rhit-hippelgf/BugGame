e6638e2ab01bc51de311d196aa0190c1d0dd20b3
carioltl <carioltl@rose-hulman.edu>
2025-05-19 15:33:02 -0400

    fixed constructors

--- BugGame/src/roomStuff/BossRoom.java
+++ BugGame/src/roomStuff/BossRoom.java
@@ -2,8 +2,8 @@ package roomStuff;
 
 public class BossRoom extends Room {
 
-	public BossRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize) {
-		super(layout, north, east, south, west, tileSize);
+	public BossRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize, int level) {
+		super(layout, north, east, south, west, tileSize, level);
 		// TODO Auto-generated constructor stub
 	}
 
--- BugGame/src/roomStuff/ItemRoom.java
+++ BugGame/src/roomStuff/ItemRoom.java
@@ -2,8 +2,8 @@ package roomStuff;
 
 public class ItemRoom extends Room {
 
-	public ItemRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize) {
-		super(layout, north, east, south, west, tileSize);
+	public ItemRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize, int level) {
+		super(layout, north, east, south, west, tileSize, level);
 		// TODO Auto-generated constructor stub
 	}
 
