9d127a1b775cb39fba5d9676c148efdc70c7477e
rumzissc <rumzissc@rose-hulman.edu>
2025-05-19 16:02:17 -0400

    fixed hole and rock

--- BugGame/src/roomComponents/Hole.java
+++ BugGame/src/roomComponents/Hole.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 
 public class Hole extends Tile {
 
-	public Hole(int x1, int y1) {
-		super(x1, y1);
+	public Hole(int x1, int y1, int level) {
+		super(x1, y1, level);
 		super.color = Color.GRAY;
 	}
 }
--- BugGame/src/roomComponents/Rock.java
+++ BugGame/src/roomComponents/Rock.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 
 public class Rock extends Hole {
 
-	public Rock(int x1, int y1) {
-		super(x1, y1);
+	public Rock(int x1, int y1, int level) {
+		super(x1, y1, level);
 		super.color = Color.BLACK;
 	}
 
