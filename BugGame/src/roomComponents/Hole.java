7da7c1909abb31ba983ab72c8f5dfdf7bd6eaa5a
carioltl <carioltl@rose-hulman.edu>
2025-05-19 15:05:52 -0400

    nothing

--- BugGame/src/roomComponents/Hole.java
+++ BugGame/src/roomComponents/Hole.java
@@ -2,7 +2,7 @@ package roomComponents;
 
 import java.awt.Color;
 
-public class Hole extends Tile{
+public class Hole extends Tile {
 
 	public Hole(int x1, int y1) {
 		super(x1, y1);
--- BugGame/src/roomComponents/Rock.java
+++ BugGame/src/roomComponents/Rock.java
@@ -2,7 +2,7 @@ package roomComponents;
 
 import java.awt.Color;
 
-public class Rock extends Hole{
+public class Rock extends Hole {
 
 	public Rock(int x1, int y1) {
 		super(x1, y1);
