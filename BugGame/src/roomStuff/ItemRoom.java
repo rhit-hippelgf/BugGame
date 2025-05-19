7d8e36cb669bb9f5696e98b58c28e73ee5fb76f1
carioltl <carioltl@rose-hulman.edu>
2025-05-19 16:13:51 -0400

    fixed error

--- BugGame/src/roomStuff/ItemRoom.java
+++ BugGame/src/roomStuff/ItemRoom.java
@@ -2,8 +2,8 @@ package roomStuff;
 
 public class ItemRoom extends Room {
 
-	public ItemRoom(char[][] layout, boolean north, boolean east, boolean south, boolean west, int tileSize, int level) {
-		super(layout, north, east, south, west, tileSize, level);
+	public ItemRoom(boolean north, boolean east, boolean south, boolean west, int tileSize, int level) {
+		super(north, east, south, west, tileSize, level);
 		// TODO Auto-generated constructor stub
 	}
 
