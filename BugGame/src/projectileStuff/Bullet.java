9e13904f335a0b7fd6b1388f76ea5c851f5f64b1
rumzissc <rumzissc@rose-hulman.edu>
2025-05-22 13:38:27 -0400

    added sfx to assets folder

--- BugGame/src/projectileStuff/Bullet.java
+++ BugGame/src/projectileStuff/Bullet.java
@@ -2,7 +2,12 @@ package projectileStuff;
 
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.io.File;
 import java.util.List;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 import creatureStuff.Creature;
 import effectStuff.Effect;
@@ -36,6 +41,7 @@ public abstract class Bullet {
 
         this.xspeed = speed * this.dx;
         this.yspeed = speed * this.dy;
+        playShootSound();
     }
 
     public void update() {
@@ -92,4 +98,16 @@ public abstract class Bullet {
     }
 
     public abstract void draw(Graphics g);
+    
+    private void playShootSound() {
+        try {
+          AudioInputStream a = AudioSystem.getAudioInputStream(
+              new File("assets/sounds/pop.mp3"));
+          Clip clip = AudioSystem.getClip();
+          clip.open(a);
+          clip.start();
+        } catch (Exception e) {
+          System.err.println("Error playing sound: " + e.getMessage());
+        }
+    }
 }
