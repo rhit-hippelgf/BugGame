cf40a073c5d8217d7fc5651643a71937a4cb796a
hippelgf <hippelgf@rose-hulman.edu>
2025-05-25 20:06:49 -0400

    added OneUp item, not implemented yet

---
+++ BugGame/src/itemStuff/OneUp.java
@@ -0,0 +1,32 @@
+package itemStuff;
+
+import creatureStuff.Player;
+
+public class OneUp extends Item {
+
+    private static final int STAT_BOOST = 1;
+
+    public OneUp(int x, int y) {
+        super(x, y, "assets/sprites/items/Honey.png", STAT_BOOST);  
+    }
+
+    @Override
+    public void applyEffect(Player player) {
+        player.addItem(this);  
+    }
+
+    @Override
+    public String getStatCat() {
+        return "lives";
+    }
+
+    @Override
+    public int getStatBoost() {
+        return STAT_BOOST;
+    }
+
+    @Override
+    public Rarity getRarity() {
+        return Rarity.EPIC; 
+    }
+}
